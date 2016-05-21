a8844a990fff37c8ce0eda46321cf0631be80d71
Phong <p7157568@gmail.com>
2016-05-21 07:30:24 +0700

    complete tut 2 make project

---
+++ app/src/main/java/dhbk/android/busycode/EmPubLiteActivity.java
@@ -0,0 +1,13 @@
+package dhbk.android.busycode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmPubLiteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_em_pub_lite);
+    }
+}
